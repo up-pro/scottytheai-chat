5b928e4d2714ca33ef424e0408434281fcd933fd
mr-new-0509 <cliffahleongtech@gmail.com>
2023-07-28 02:43:51 +0900

    ChatBox

--- src/pages/Chat/ChatBox.tsx
+++ src/pages/Chat/ChatBox.tsx
@@ -1,25 +1,61 @@
-import { FormEvent, ChangeEvent, useState } from 'react';
-import { Button, Box, Grid, Stack, TextField } from "@mui/material";
+import { FormEvent, ChangeEvent, useState, useRef, useEffect } from 'react';
+import { Button, Box, Grid, Stack, TextField, Avatar, Typography } from "@mui/material";
 import { grey } from "@mui/material/colors";
-// import { IChat } from '../../utils/interfaces';
+import { IChat } from '../../utils/interfaces';
 
 export default function ChatBox() {
+  const chatBoxRef = useRef<HTMLDivElement | null>(null)
+
   const [question, setQuestion] = useState<string>('')
-  // const [chats, setChats] = useState<Array<IChat>>([])
-  // const [gptIsLoading, setGptIsLoading] = useState<boolean>(false)
+  const [chats, setChats] = useState<Array<IChat>>([])
+  const [gptIsLoading] = useState<boolean>(false)
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log('>>>>>>> question => ', question)
+
+    const newChat: IChat = {
+      id: chats.length,
+      sender: 'user',
+      message: question
+    }
+
+    if (!gptIsLoading) {
+      setChats([...chats, newChat])
+    }
   }
+
+  useEffect(() => {
+    const lastChildElement = chatBoxRef.current?.lastElementChild
+    if (lastChildElement) {
+      lastChildElement?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [chats])
 
   return (
     <>
-      <Stack flexGrow={1} spacing={1}>
-
+      {/* Chatbox */}
+      <Stack flexGrow={1} spacing={2} px={3} sx={{ overflowY: 'auto', height: '100px' }} ref={chatBoxRef}>
+        {chats.map(chat => (
+          <Stack direction="row" spacing={1} key={chat.id}>
+            {chat.sender === 'gpt' ? (
+              <Avatar
+                src="/assets/images/gpt.png"
+                alt="GPT"
+              />
+            ) : (
+              <Avatar
+                src="/assets/images/user.png"
+                alt="User"
+              />
+            )}
+            <Typography component="p" color={grey[100]} fontSize={18}>
+              {chat.message}
+            </Typography>
+          </Stack>
+        ))}
       </Stack>
 
-      {/* Controller */}
+      {/* Input */}
       <Stack spacing={2}>
         <Stack direction="row" justifyContent="center">
           <Button sx={{ bgcolor: grey[800], borderRadius: 9999, px: 4 }}>Refetch</Button>
